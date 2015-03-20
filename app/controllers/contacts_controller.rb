cce4942d54287a8482259c8c89527bd682da4ab7
JoeyHilton <shnek81@gmail.com>
2015-03-20 15:49:29 -0600

    controller filled out

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,49 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def index
+    @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def create
+    @contact = Contact.new(contact_params)
+  
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @contact = Contact.find(params[:id])
   end
 
   def update
+    @contact = Contact.find(params[:id])
+  
+    if @contact.update(contact_params)
+      redirect_to contacts_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:contact).permit(:name, :email, :phone_number)
   end
 end
